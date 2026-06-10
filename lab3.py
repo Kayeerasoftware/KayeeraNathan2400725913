80d6789b29bf8572238792682ff4fa38e0c06a0b
Kayeerasoftware <kayeerasoftware@gmail.com>
2026-06-10 14:42:55 +0300

    the recess folder update

---
+++ lab3.py
@@ -0,0 +1,5 @@
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+print("-" * 50)
+print(f"The area of the rectangle for the length {length} and width {width} is: {length * width}")
